5f971489d7c0c53374b80bb8a8763f0e0ddb97eb
Vishnu Sunil <vishnusunil243@gmail.com>
2024-03-10 13:34:59 +0530

    added job management

--- pb/company_grpc.pb.go
+++ pb/company_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -25,6 +26,10 @@ type CompanyServiceClient interface {
 	CompanySignup(ctx context.Context, in *CompanySignupRequest, opts ...grpc.CallOption) (*CompanySignupResponse, error)
 	CompanyLogin(ctx context.Context, in *CompanyLoginRequest, opts ...grpc.CallOption) (*CompanySignupResponse, error)
 	AddJobs(ctx context.Context, in *AddJobRequest, opts ...grpc.CallOption) (*JobResponse, error)
+	UpdateJobs(ctx context.Context, in *UpdateJobRequest, opts ...grpc.CallOption) (*JobResponse, error)
+	DeleteJobs(ctx context.Context, in *GetJobById, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	GetAllJobs(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (CompanyService_GetAllJobsClient, error)
+	GetJob(ctx context.Context, in *GetJobById, opts ...grpc.CallOption) (*JobResponse, error)
 }
 
 type companyServiceClient struct {
@@ -62,6 +67,65 @@ func (c *companyServiceClient) AddJobs(ctx context.Context, in *AddJobRequest, o
 	return out, nil
 }
 
+func (c *companyServiceClient) UpdateJobs(ctx context.Context, in *UpdateJobRequest, opts ...grpc.CallOption) (*JobResponse, error) {
+	out := new(JobResponse)
+	err := c.cc.Invoke(ctx, "/user.CompanyService/UpdateJobs", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *companyServiceClient) DeleteJobs(ctx context.Context, in *GetJobById, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, "/user.CompanyService/DeleteJobs", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *companyServiceClient) GetAllJobs(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (CompanyService_GetAllJobsClient, error) {
+	stream, err := c.cc.NewStream(ctx, &CompanyService_ServiceDesc.Streams[0], "/user.CompanyService/GetAllJobs", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &companyServiceGetAllJobsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type CompanyService_GetAllJobsClient interface {
+	Recv() (*JobResponse, error)
+	grpc.ClientStream
+}
+
+type companyServiceGetAllJobsClient struct {
+	grpc.ClientStream
+}
+
+func (x *companyServiceGetAllJobsClient) Recv() (*JobResponse, error) {
+	m := new(JobResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *companyServiceClient) GetJob(ctx context.Context, in *GetJobById, opts ...grpc.CallOption) (*JobResponse, error) {
+	out := new(JobResponse)
+	err := c.cc.Invoke(ctx, "/user.CompanyService/GetJob", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CompanyServiceServer is the server API for CompanyService service.
 // All implementations must embed UnimplementedCompanyServiceServer
 // for forward compatibility
@@ -69,6 +133,10 @@ type CompanyServiceServer interface {
 	CompanySignup(context.Context, *CompanySignupRequest) (*CompanySignupResponse, error)
 	CompanyLogin(context.Context, *CompanyLoginRequest) (*CompanySignupResponse, error)
 	AddJobs(context.Context, *AddJobRequest) (*JobResponse, error)
+	UpdateJobs(context.Context, *UpdateJobRequest) (*JobResponse, error)
+	DeleteJobs(context.Context, *GetJobById) (*emptypb.Empty, error)
+	GetAllJobs(*emptypb.Empty, CompanyService_GetAllJobsServer) error
+	GetJob(context.Context, *GetJobById) (*JobResponse, error)
 	mustEmbedUnimplementedCompanyServiceServer()
 }
 
@@ -84,6 +152,18 @@ func (UnimplementedCompanyServiceServer) CompanyLogin(context.Context, *CompanyL
 }
 func (UnimplementedCompanyServiceServer) AddJobs(context.Context, *AddJobRequest) (*JobResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddJobs not implemented")
+}
+func (UnimplementedCompanyServiceServer) UpdateJobs(context.Context, *UpdateJobRequest) (*JobResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateJobs not implemented")
+}
+func (UnimplementedCompanyServiceServer) DeleteJobs(context.Context, *GetJobById) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteJobs not implemented")
+}
+func (UnimplementedCompanyServiceServer) GetAllJobs(*emptypb.Empty, CompanyService_GetAllJobsServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetAllJobs not implemented")
+}
+func (UnimplementedCompanyServiceServer) GetJob(context.Context, *GetJobById) (*JobResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetJob not implemented")
 }
 func (UnimplementedCompanyServiceServer) mustEmbedUnimplementedCompanyServiceServer() {}
 
@@ -152,6 +232,81 @@ func _CompanyService_AddJobs_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CompanyService_UpdateJobs_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateJobRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CompanyServiceServer).UpdateJobs(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/user.CompanyService/UpdateJobs",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CompanyServiceServer).UpdateJobs(ctx, req.(*UpdateJobRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CompanyService_DeleteJobs_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetJobById)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CompanyServiceServer).DeleteJobs(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/user.CompanyService/DeleteJobs",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CompanyServiceServer).DeleteJobs(ctx, req.(*GetJobById))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CompanyService_GetAllJobs_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(emptypb.Empty)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(CompanyServiceServer).GetAllJobs(m, &companyServiceGetAllJobsServer{stream})
+}
+
+type CompanyService_GetAllJobsServer interface {
+	Send(*JobResponse) error
+	grpc.ServerStream
+}
+
+type companyServiceGetAllJobsServer struct {
+	grpc.ServerStream
+}
+
+func (x *companyServiceGetAllJobsServer) Send(m *JobResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func _CompanyService_GetJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetJobById)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CompanyServiceServer).GetJob(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/user.CompanyService/GetJob",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CompanyServiceServer).GetJob(ctx, req.(*GetJobById))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // CompanyService_ServiceDesc is the grpc.ServiceDesc for CompanyService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -171,7 +326,25 @@ var CompanyService_ServiceDesc = grpc.ServiceDesc{
 			MethodName: "AddJobs",
 			Handler:    _CompanyService_AddJobs_Handler,
 		},
+		{
+			MethodName: "UpdateJobs",
+			Handler:    _CompanyService_UpdateJobs_Handler,
+		},
+		{
+			MethodName: "DeleteJobs",
+			Handler:    _CompanyService_DeleteJobs_Handler,
+		},
+		{
+			MethodName: "GetJob",
+			Handler:    _CompanyService_GetJob_Handler,
+		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "GetAllJobs",
+			Handler:       _CompanyService_GetAllJobs_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "company.proto",
 }
